06594e3cdfb2d32efdcd83bb1bc43f6c35cdf439
Victor <36416236+victor-yunenko@users.noreply.github.com>
2023-12-29 07:33:49 -0800

    fix: limit Gifts access by User

--- server/gifts/apps/gifts/graphql/types.py
+++ server/gifts/apps/gifts/graphql/types.py
@@ -1,30 +1,49 @@
 import strawberry
 import strawberry_django
+from django.db.models import QuerySet
 from strawberry.types import Info
 
 from gifts.apps.auth.graphql.types import UserType
-from gifts.apps.gifts import models
+from gifts.apps.gifts.models import Gift
+from gifts.apps.gifts.models import GiftImage
+from gifts.apps.gifts.models import GiftOrder
 
 
-@strawberry_django.type(models.Gift, fields="__all__")
+@strawberry_django.type(Gift, fields="__all__")
 class GiftType:
+    @classmethod
+    def get_queryset(cls, queryset: QuerySet[Gift], info: Info) -> QuerySet[Gift]:
+        return queryset.filter(user=info.context.request.user)
+
     @strawberry.field()
-    def is_accepted(self: models.Gift, info: Info) -> bool:
+    def is_accepted(self: Gift, info: Info) -> bool:
         return self.orders.filter(user=info.context.request.user).aexists()
 
 
-@strawberry_django.type(models.GiftOrder, fields="__all__")
+@strawberry_django.type(GiftOrder, fields="__all__")
 class GiftOrderType:
     gift: GiftType
     user: UserType
 
+    @classmethod
+    def get_queryset(cls, queryset: QuerySet[GiftOrder], info: Info) -> QuerySet[GiftOrder]:
+        return queryset.filter(user=info.context.request.user)
 
-@strawberry_django.input(models.GiftOrder, fields="__all__")
+
+@strawberry_django.input(GiftOrder, fields="__all__")
 class GiftOrderInput:
     gift: GiftType
     user: UserType
 
+    @classmethod
+    def get_queryset(cls, queryset: QuerySet[GiftImage], info: Info) -> QuerySet[GiftImage]:
+        return queryset.filter(user=info.context.request.user)
 
-@strawberry_django.type(models.GiftImage, fields="__all__")
+
+@strawberry_django.type(GiftImage, fields="__all__")
 class GiftImageType:
     gift: GiftType
+
+    @classmethod
+    def get_queryset(cls, queryset: QuerySet[GiftImage], info: Info) -> QuerySet[GiftImage]:
+        return queryset.filter(gift__user=info.context.request.user)
