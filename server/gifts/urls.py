e279788e92d1113b4a39f9f76073f49d75af2b16
Victor <36416236+victor-yunenko@users.noreply.github.com>
2024-01-07 19:47:29 -0800

    fix: serve media/ in prod
    use insecure django.views.static.serve, as i'm lazy to spin up a bucket

--- server/gifts/urls.py
+++ server/gifts/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.db import DEFAULT_DB_ALIAS
 from django.db import connections
@@ -7,6 +6,8 @@ from django.db.migrations.executor import MigrationExecutor
 from django.http import HttpResponse
 from django.urls import include
 from django.urls import path
+from django.urls import re_path
+from django.views.static import serve
 from strawberry.django.views import AsyncGraphQLView
 
 from gifts.graphql.schema import schema
@@ -40,8 +41,14 @@ urlpatterns = [
         ),
     ),
     path("healthcheck/", healthcheck_view),
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {
+            "document_root": settings.MEDIA_ROOT,
+        },
+    ),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
